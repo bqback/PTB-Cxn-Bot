0ed70cfe414cbd00337e600b962a9f0728522e74
Mike WInfield <lolwut-lol@yandex.ru>
2024-06-08 17:05:31 +0300

    GOOOOOOOOL fix

--- components/callbacks/fun.py
+++ components/callbacks/fun.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import ContextTypes
+from telegram.constants import DiceEmoji
 
-from components.constants import constants
+# from components.constants import constants
 
 async def goal(update: Update, _: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(constants.SOCCER_BALL)
+    await update.message.reply_dice(emoji=DiceEmoji.FOOTBALL)
